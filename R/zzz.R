026d1787dad10200f3602e45cb617eed04a914a7
Peter Solymos <psolymos@gmail.com>
2022-04-11 13:31:36 -0600

    Add config for log rotation
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
 .onLoad <- function(libname, pkgname) {
   set_logfile(confirm = FALSE)
   set_timestamp_format(confirm = FALSE)
+  set_rotate_lines(NULL, confirm = FALSE)
+  invisible(NULL)
+}
+
+.onUnload <- function(libpath) {
+  for (i in names(.config)) {
+    .config[[i]] <- NULL
+  }
+  invisible(NULL)
 }
