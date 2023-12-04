9f12292373a280289d1de2ec4b82b1c56321edce
MEO265 <99362508+MEO265@users.noreply.github.com>
2023-12-04 20:50:09 +0100

    feat: Use fixed matches in `sanitizer`

--- R/json.R
+++ R/json.R
@@ -35,8 +35,8 @@
 default_ndjson_sanitizer <- function(string, sanitize = TRUE) {
   # String map; will dispatch left-vs.-right replacement based on `sanitize` bool
   map <- list(
-    "\\{" = "__LEFTBRACE__",
-    "\\}" = "__RIGHTBRACE__",
+    "{" = "__LEFTBRACE__",
+    "}" = "__RIGHTBRACE__",
     '"' = "__DBLQUOTE__",
     "," = "__COMMA__",
     "\r" = "__CR__",
@@ -45,9 +45,9 @@ default_ndjson_sanitizer <- function(string, sanitize = TRUE) {
   
   for (k in names(map)) {
     if (sanitize) {
-      string <- gsub(k, map[k], string)
+      string <- gsub(k, map[k], string, fixed = TRUE)
     } else {
-      string <- gsub(map[k], k, string)
+      string <- gsub(map[k], k, string, fixed = TRUE)
     }
   }
   
